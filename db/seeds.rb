515a6f0937cacac82a2590cdcd453cdd6a6e4922
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-04 17:38:42 -0600

    Add figther seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,21 @@ tmmaf = Gym.create!(name: "Texeira MMA & Fitness",  number_of_champions: 2, hold
 topteamfl = Gym.create!(name: "American Top Team, Cocunut Creek",  number_of_champions: 6, holds_current_champion: true)
 sbgire = Gym.create!(name: "Straight Blast Gym, Ireland",  number_of_champions: 1, holds_current_champion: false)
 
+khabib = Fighter.create!(name: "Khabib Nurmagomedov", age: 35, style: "Combat Sambo", active: false, gym_id: akasj)
+islam = Fighter.create!(name: "Islam Makhachev", age: 32, style: "Combat Sambo", active: true, gym_id: akasj)
+dc = Fighter.create!(name: "Daniel Cormier", age: 45, style: "Wrestling", active: false, gym_id: akasj)
+leon = Fighter.create!(name: "Leon Edwards", age: 32, style: "Striker", active: true, gym_id: akasj)
+
+teixeira = Fighter.create!(name: "Glover Teixeira", age: 44, style: "Brazilian Jiu-Jitsu", active: false, gym_id: tmmaf)
+pereira = Fighter.create!(name: "Alex Pereira", age: 37, style: "Kickboxing", active: false, gym_id: tmmaf)
+
+pantoja = Fighter.create!(name: "Alexandre Patoja", age: 34, style: "Brazilian Jiu-Jitsu", active: true, gym_id: topteamfl)
+woodley = Fighter.create!(name: "Tyron Woodley", age: 42, style: "Wrestling", active: false, gym_id: topteamfl)
+yoel = Fighter.create!(name: "Yoel Romero", age: 47, style: "Wrestling", active: false, gym_id: topteamfl)
+evloev = Fighter.create!(name: "Movsar Evloev", age: 30, style: "Freestyle", active: true, gym_id: topteamfl)
+
+conor = Fighter.create!(name: "Conor McGregor", age: 35, style: "Striker", active: false, gym_id: sbgire)
+
 puts ""
 puts "Successfully seeded."
 puts ""
