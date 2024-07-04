79a338a0aff133bd09fac30ea0efcf7cbdebc513
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-04 13:17:59 -0600

    Add changes to seed file to destroy old data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Gym.destroy_all
+Fighter.destroy_all
 
 akasj = Gym.create!(name: "American Kickboxing Academy, San Jose", number_of_champions: 6, holds_current_champion: true)
 tmmaf = Gym.create!(name: "Texeira MMA & Fitness",  number_of_champions: 2, holds_current_champion: true)
 topteamfl = Gym.create!(name: "American Top Team, Cocunut Creek",  number_of_champions: 6, holds_current_champion: true)
 sbgire = Gym.create!(name: "Straight Blast Gym, Ireland",  number_of_champions: 1, holds_current_champion: false)
 
+puts ""
 puts "Successfully seeded."
+puts ""
