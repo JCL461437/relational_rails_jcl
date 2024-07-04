04dbe4bc92258029491154872a844bc7773b14dc
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-04 12:55:38 -0600

    Update gyms index and show spex test data

--- spec/features/gyms/show_spec.rb
+++ spec/features/gyms/show_spec.rb
@@ -1,4 +1,26 @@
 require "rails_helper"
 
 RSpec.describe 'Gyms show' do
+  
+  before :each do
+    @akasj = Gym.create!(name: "American Kickboxing Academy, San Jose", number_of_champions: 6, holds_current_champion: true)
+    @tmmaf = Gym.create!(name: "Texeira MMA & Fitness",  number_of_champions: 2, holds_current_champion: true)
+    @topteamfl = Gym.create!(name: "American Top Team, Cocunut Creek",  number_of_champions: 6, holds_current_champion: true)
+    @sbgire = Gym.create!(name: "Straight Blast Gym, Ireland",  number_of_champions: 1, holds_current_champion: false)
+  end
+
+  describe 'as a visitor' do
+    describe 'when I visit /gyms/:id' do 
+      it 'displays the gyms with its id and attributes' do
+        
+        visit "/gyms/#{}"
+
+        expect(page).to have_content("American Kickboxing Academy, San Jose")
+        expect(page).to have_content("Texeira MMA & Fitness")
+        expect(page).to have_content("American Top Team, Cocunut Creek")
+        expect(page).to have_content("Straight Blast Gym, Ireland")
+
+        # save_and_open_page
+      end
+    end
 end
