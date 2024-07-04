5f82f66590a9153fa7e40c6f0ee81a6bba08d9e8
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-04 12:03:26 -0600

    Refactor file extension for gym and fighter feature tests from .html.erb to _spec.rb

--- spec/features/gyms/index_spec.rb
+++ spec/features/gyms/index_spec.rb
@@ -3,15 +3,16 @@ require "rails_helper"
 RSpec.describe 'Gyms index' do
 
   before :each do
-    @akasj = Gym.create!(name: 'American Kickboxing Academy, San Jose')
-    @tmma = Gym.create! (name: 'Texeira MMA & Fitness')
-    @amertopteamfl = Gym.create!(name: 'American Top Team, Cocunut Creek')
-    @sbgire = Gym.create!(name: 'Straight Blast Gym, Ireland')
+    @akasj = Gym.create!(name: "American Kickboxing Academy, San Jose")
+    @tmmaf = Gym.create!(name: "Texeira MMA & Fitness")
+    @topteamfl = Gym.create!(name: "American Top Team, Cocunut Creek")
+    @sbgire = Gym.create!(name: "Straight Blast Gym, Ireland")
   end
 
   describe 'as a visitor' do
     describe 'when I visit /gyms' do 
       it 'displays the name of each parent record in the system' do
+        
         visit "/gyms"
 
         expect(page).to have_content("American Kickboxing Academy, San Jose")
