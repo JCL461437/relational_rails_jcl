592389594f2fb40f50d5b15177f67e00503bab31
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-07 17:09:20 -0600

    Add new and create actions in GymFightersController

--- app/controllers/gym_fighters_controller.rb
+++ app/controllers/gym_fighters_controller.rb
@@ -3,4 +3,18 @@ class GymFightersController < ApplicationController
     @gym = Gym.find(params[:id])
     @fighters = @gym.fighters
   end
+
+  def new
+  end
+  
+  def create
+    @gym = Gym.find(params[:id])
+    @new_fighter = @gym.fighter.create({ name: params[:name], number_of_champions: params[:number_of_champions],
+    holds_current_champion: params[:holds_current_champion]}) 
+    # you could do this long params or use the strong params option
+    
+    @new_fighter.save
+    
+    redirect_to "/gyms/#{@gym.id}/fighters"
+  end
 end
