e88fd58ccc6ba5a68419d956ea30c175d6ce4f29
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-07 17:27:45 -0600

    Added updates to index.html.erb view for gyms/fighters. Added new and create actions in the gyms fighters controller.

--- app/controllers/gym_fighters_controller.rb
+++ app/controllers/gym_fighters_controller.rb
@@ -5,12 +5,13 @@ class GymFightersController < ApplicationController
   end
 
   def new
+    @gym = Gym.find(params[:id])
   end
   
   def create
     @gym = Gym.find(params[:id])
-    @new_fighter = @gym.fighter.create({ name: params[:name], number_of_champions: params[:number_of_champions],
-    holds_current_champion: params[:holds_current_champion]}) 
+    @new_fighter = Fighter.create({ name: params[:name], age: params[:age],
+    style: params[:style], active: params[:active], gym: @gym}) 
     # you could do this long params or use the strong params option
     
     @new_fighter.save
