7036925351851e1b5d27ed127b5955df3fd69dae
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-07 17:47:02 -0600

    Add edit and update actions to the fighter/controller

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -3,6 +3,16 @@ class FightersController < ApplicationController
     @fighters = Fighter.all
   end
 
+  def edit
+    @fighter = Fighter.find(params[:id])
+  end
+
+  def update
+    fighter = Fighter.find(params[:id])
+    fighter.update(:name, :age, :style, :active) #use of gym_params below instead of the long one above to visualize difference
+    redirect_to "/fighters/#{fighter.id}"
+  end
+
   def show
     @fighter = Fighter.find(params[:id])
   end
